13e58b9ebeb0f410bd272355dee3a4ed3172cce2
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-03-15 23:31:09 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const startServer = async () => {
     });
 
     app.listen(PORT, () => {
-      console.log(`Server runing on ${PORT}`);
+      console.log(`Server runing on ${PORT}!`);
     });
   } catch (error) {
     console.error("Connection error", error);
